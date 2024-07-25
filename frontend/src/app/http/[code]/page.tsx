7307eb5f8b4adbf09a7bf35643cd978a64253028
sinclairr08 <ljhjoon00@gmail.com>
2024-07-25 20:47:23 +0900

    feat: add back button at http detail page

--- frontend/src/app/http/[code]/page.tsx
+++ frontend/src/app/http/[code]/page.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import { HttpImage } from "../page";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
 
 interface IHttpCode {
   message: string;
@@ -33,7 +34,7 @@ const CodePage: React.FC = () => {
 
   return (
     httpCodeInfo && (
-      <div className="flex flex-col justify-center items-center bg-red-400 py-24 space-y-8">
+      <div className="flex flex-col justify-center items-center bg-red-400 pt-24 pb-12 space-y-2">
         <div className="text-center text-white font-bold">{code}</div>
         <div className="text-center text-white font-bold">
           {httpCodeInfo.message}
@@ -42,6 +43,9 @@ const CodePage: React.FC = () => {
         <div className="text-center text-white font-bold  whitespace-pre-line">
           {httpCodeInfo.description}
         </div>
+        <Link href="/http">
+          <button className="bg-white p-2 rounded-md">back</button>
+        </Link>
       </div>
     )
   );
