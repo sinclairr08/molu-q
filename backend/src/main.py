b2cf5749e8aea6750b67b9eb3bec5f7103f0bd20
sinclairr08 <ljhjoon00@gmail.com>
2024-07-29 00:38:10 +0900

    feat: set response type explictly

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,7 +3,28 @@ import json
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
+from pydantic import BaseModel
+from typing import Optional
+
 app = FastAPI()
+
+class QuizResponse(BaseModel):
+    problemNo: int
+    problemType: str
+    question: str
+    musicPath: Optional[str]
+    answerList: Optional[list[str]]
+    answer: str
+
+class HttpResponse(BaseModel):
+    code: int
+    message: str
+    ext: str
+
+class HttpDetailResponse(BaseModel):
+    message: str
+    ext: str
+    description: str
 
 async def read_file(fn):
     try:
@@ -14,20 +35,19 @@ async def read_file(fn):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.get("/quiz")
+@app.get("/quiz", response_model=list[QuizResponse])
 async def read_quiz():
     data = await read_file(f"{os.path.dirname(__file__)}/local/quiz.json")
     return JSONResponse(content=data)
-        
 
-@app.get("/http")
+
+@app.get("/http", response_model=list[HttpResponse])
 async def read_http():
     data = await read_file(f"{os.path.dirname(__file__)}/local/http.json")
     return JSONResponse(content=data)
 
-    
-@app.get("/http/{code}")
+
+@app.get("/http/{code}", response_model=HttpDetailResponse)
 async def read_http_detail(code: int):
     data = await read_file(f"{os.path.dirname(__file__)}/local/http/{code}.json")
     return JSONResponse(content=data)
-
