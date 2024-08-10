3ee7e32c177501bb33353d38572aa7d27e52e5f6
sinclairr08 <ljhjoon00@gmail.com>
2024-08-11 01:38:34 +0900

    refactor: change useless drop code

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,28 +1,18 @@
-from typing import Union
-
 from pymongo import MongoClient
-from pymongo.cursor import Cursor
 
 client = MongoClient("mongodb://localhost:27017")
 db = client["molu-q"]
 
 
-def drop_id(documents: Union[list, Cursor]) -> list:
-    return [{k: v for k, v in doc.items() if k != "_id"} for doc in documents]
-
-
 def read_quiz():
     collection = db["quiz_ids"]
-    documents = collection.find({})
 
-    return drop_id(documents)
+    return collection.find({}, {"_id": 0})
 
 
 def read_quiz_by_id(quiz_id: int):
     collection = db["quiz"]
     if quiz_id == 0:
-        documents = collection.find({})
-    else:
-        documents = collection.find({"quiz_id": quiz_id})
+        return collection.find({}, {"_id": 0})
 
-    return drop_id(documents)
+    return collection.find({"quiz_id": quiz_id}, {"_id": 0})
