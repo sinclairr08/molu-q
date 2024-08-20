c710951048dc96453ccff328dc51171058bc2efe
sinclairr08 <ljhjoon00@gmail.com>
2024-08-21 00:11:57 +0900

    feat: add select item addition function for new quiz

--- frontend/src/app/quiz/new/page.tsx
+++ frontend/src/app/quiz/new/page.tsx
@@ -3,6 +3,7 @@
 import { SelectBox, SubmitButton } from "@/components/general";
 import { ShortInput } from "@/components/quiz/ShortAnswer";
 import axios from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export interface IQuizInputForm {
@@ -10,11 +11,21 @@ export interface IQuizInputForm {
   problemId: number;
   problemType: string;
   question: string;
+  selectList?: string[];
   answer: string;
 }
 
 const QuizAddPage: React.FC = () => {
-  const { register, handleSubmit } = useForm<IQuizInputForm>();
+  const { register, handleSubmit, watch } = useForm<IQuizInputForm>();
+  const [selectItems, setSelectItems] = useState<string[]>();
+  const addSelectItem = () => {
+    const idx = selectItems ? selectItems.length : 0;
+    setSelectItems((prev) =>
+      prev ? [...prev, `객관식 ${idx + 1}`] : [`객관식 ${idx + 1}`],
+    );
+  };
+
+  const currentProblemType = watch("problemType");
 
   const isValid = (data: IQuizInputForm) => {
     if (
@@ -61,6 +72,28 @@ const QuizAddPage: React.FC = () => {
           <span className="px-2">문제</span>
           <ShortInput register={register("question")} />
         </label>
+        {currentProblemType &&
+          currentProblemType.toLowerCase().includes("select") && (
+            <>
+              {selectItems &&
+                selectItems.map((selectItem, index) => (
+                  <label
+                    key={selectItem}
+                    className="flex items-center justify-center"
+                  >
+                    <span className="px-2">{selectItem}</span>
+                    <ShortInput register={register(`selectList.${index}`)} />
+                  </label>
+                ))}
+
+              <label
+                className="flex items-center justify-center cursor-pointer"
+                onClick={addSelectItem}
+              >
+                +
+              </label>
+            </>
+          )}
         <label className="flex items-center justify-center">
           <span className="px-2">답</span>
           <ShortInput register={register("answer")} />
