0ccd36362f27881efa34b883b730e9d6b3159abb
sinclairr08 <ljhjoon00@gmail.com>
2024-09-02 00:27:15 +0900

    feat: add file input component at quiz page

--- frontend/src/app/quiz/new/page.tsx
+++ frontend/src/app/quiz/new/page.tsx
@@ -13,6 +13,7 @@ export interface IQuizInputForm {
   question: string;
   selectList?: string[];
   answer: string;
+  image?: FileList;
 }
 
 interface IShortInputRow {
@@ -31,6 +32,26 @@ const ShortInputRow = ({ register, label }: IShortInputRow) => {
     <label className="flex items-center">
       <span className="px-2 w-24">{label}</span>
       <ShortInput register={register} />
+    </label>
+  );
+};
+
+const FileInputRow = ({ register, label }: IShortInputRow) => {
+  return (
+    <label className="flex items-center">
+      <span className="px-2 w-24">{label}</span>
+      <label htmlFor="file-upload">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="w-48 h-48 fill-current text-cyan-600 cursor-pointer"
+        >
+          <path d="M19,0H5A5.006,5.006,0,0,0,0,5V19a5.006,5.006,0,0,0,5,5H19a5.006,5.006,0,0,0,5-5V5A5.006,5.006,0,0,0,19,0ZM5,2H19a3,3,0,0,1,3,3V19a2.951,2.951,0,0,1-.3,1.285l-9.163-9.163a5,5,0,0,0-7.072,0L2,14.586V5A3,3,0,0,1,5,2ZM5,22a3,3,0,0,1-3-3V17.414l4.878-4.878a3,3,0,0,1,4.244,0L20.285,21.7A2.951,2.951,0,0,1,19,22Z" />
+          <path d="M16,10.5A3.5,3.5,0,1,0,12.5,7,3.5,3.5,0,0,0,16,10.5Zm0-5A1.5,1.5,0,1,1,14.5,7,1.5,1.5,0,0,1,16,5.5Z" />
+        </svg>
+
+        <input id="file-upload" type="file" {...register} className="sr-only" />
+      </label>
     </label>
   );
 };
@@ -65,6 +86,12 @@ const QuizAddPage: React.FC = () => {
       return;
     }
 
+    const formData = new FormData();
+    if (data.image && data.image.length > 0) {
+      formData.append("problemId", String(data.problemId));
+      formData.append("image", data.image[0]);
+    }
+
     const updatedData = {
       ...data,
       quizSetId: data.quizSetId ? Number(data.quizSetId) : 0,
@@ -75,6 +102,12 @@ const QuizAddPage: React.FC = () => {
       axios.post("/api/v0/quiz", updatedData, {
         headers: {
           "Content-Type": "application/json",
+        },
+      });
+
+      axios.post("/api/v0/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
         },
       });
 
@@ -124,6 +157,7 @@ const QuizAddPage: React.FC = () => {
             </>
           )}
         <ShortInputRow register={register("answer")} label="답" />
+        <FileInputRow register={register("image")} label="이미지" />
         <SubmitButton />
       </div>
     </form>
