22e5917d271f810d993109e05a0016b94623c1bb
sinclairr08 <ljhjoon00@gmail.com>
2024-10-13 00:01:03 +0900

    feat: add recruit type select option

--- frontend/src/app/recruit/page.tsx
+++ frontend/src/app/recruit/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 
 const RecruitPage: React.FC = () => {
   const [cards, setCards] = useState<any[]>([]);
+  const [curRecruitType, setCurRecruitType] = useState("");
+  const recruitTypes = ["상시", "픽업"];
 
   useEffect(() => {
     setCards(
@@ -14,16 +16,26 @@ const RecruitPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center space-y-4 mt-16">
       <div className="flex space-x-4">
-        <div className="grid grid-cols-5">
-          {cards.map((card, i) => (
-            <div
-              key={i}
-              className="text-center text-xs m-2 border-gray-400 border-2"
-            >
-              {"★".repeat(card)}
-            </div>
-          ))}
-        </div>
+        {recruitTypes.map((recruitType) => (
+          <button
+            key={recruitType}
+            className={`border-2 border-gray-400 p-2 rounded-md h-16 text-xs ${recruitType === curRecruitType ? "bg-yellow-200" : ""}`}
+            onClick={() => setCurRecruitType(recruitType)}
+          >
+            <div>{recruitType} 모집</div>
+          </button>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-5">
+        {cards.map((card, i) => (
+          <div
+            key={i}
+            className="text-center text-xs m-2 border-gray-400 border-2"
+          >
+            {"★".repeat(card)}
+          </div>
+        ))}
       </div>
     </div>
   );
