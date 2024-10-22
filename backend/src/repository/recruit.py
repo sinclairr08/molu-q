c3b259b287b675b95c71c7836975ba6eee0b7f0f
sinclairr08 <ljhjoon00@gmail.com>
2024-10-23 01:18:09 +0900

    feat: connect recruit api with real data

--- backend/src/repository/recruit.py
+++ backend/src/repository/recruit.py
@@ -1,11 +1,18 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017")
+db = client["molu-q"]
+
+
 def read_recruit_probability():
+    collection = db["students"]
     star1_p = 0.785
     star2_p = 0.185
     star3_p = 0.03
 
-    star1_list = ["요시미", "아스나", "세리나"]
-    star2_list = ["아이리", "세리카", "마리"]
-    star3_list = ["카즈사", "나츠", "이오리", "레이사"]
+    star1_list = list(collection.find({"star": 1}, {"_id": 0}))
+    star2_list = list(collection.find({"star": 2}, {"_id": 0}))
+    star3_list = list(collection.find({"star": 3}, {"_id": 0}))
 
     l1 = len(star1_list)
     l2 = len(star2_list)
@@ -14,19 +21,19 @@ def read_recruit_probability():
     results = []
 
     for i, s1 in enumerate(star1_list):
-        result = {"name": s1, "star": 1, "prob": ((i + 1) / l1) * star1_p}
+        result = dict(s1)
+        result["prob"] = ((i + 1) / l1) * star1_p
         results.append(result)
 
     for i, s2 in enumerate(star2_list):
-        result = {"name": s2, "star": 2, "prob": ((i + 1) / l2) * star2_p + star1_p}
+        result = dict(s2)
+        result["prob"] = ((i + 1) / l2) * star2_p + star1_p
         results.append(result)
 
     for i, s3 in enumerate(star3_list):
-        result = {
-            "name": s3,
-            "star": 3,
-            "prob": ((i + 1) / l3) * star3_p + star1_p + star2_p,
-        }
+        result = dict(s3)
+        result["prob"] = ((i + 1) / l3) * star3_p + star1_p + star2_p
+
         results.append(result)
 
     return results
