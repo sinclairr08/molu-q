fdd3032200b1aa676293b5f0be5ae1a447afbe46
sinclairr08 <ljhjoon00@gmail.com>
2024-10-26 00:15:12 +0900

    refacor: remove duplicates at recruit api code

--- backend/src/repository/recruit.py
+++ backend/src/repository/recruit.py
@@ -6,75 +6,50 @@ db = client["molu-q"]
 
 def read_recruit_probability():
     collection = db["students"]
-    star1_p = 0.785
-    star2_p = 0.185
-    star3_p = 0.03
-
-    star1_list = list(collection.find({"star": 1}, {"_id": 0}))
-    star2_list = list(collection.find({"star": 2}, {"_id": 0}))
-    star3_list = list(collection.find({"star": 3}, {"_id": 0}))
-
-    l1 = len(star1_list)
-    l2 = len(star2_list)
-    l3 = len(star3_list)
+    star_probs = [(1, 0.785), (2, 0.185), (3, 0.03)]
 
     results = []
+    cumul_probs = 0.0
 
-    for i, s1 in enumerate(star1_list):
-        result = dict(s1)
-        result["prob"] = ((i + 1) / l1) * star1_p
-        results.append(result)
+    for star, probs in star_probs:
+        student_list = list(collection.find({"star": star}, {"_id": 0}))
+        num_student = len(student_list)
 
-    for i, s2 in enumerate(star2_list):
-        result = dict(s2)
-        result["prob"] = ((i + 1) / l2) * star2_p + star1_p
-        results.append(result)
+        for i, student in enumerate(student_list):
+            result = dict(student)
+            result["prob"] = ((i + 1) / num_student) * probs + cumul_probs
+            results.append(result)
 
-    for i, s3 in enumerate(star3_list):
-        result = dict(s3)
-        result["prob"] = ((i + 1) / l3) * star3_p + star1_p + star2_p
-
-        results.append(result)
+        cumul_probs += probs
 
     return results
 
 
 def read_pickup_probability(name):
     collection = db["students"]
-    star1_p = 0.785
-    star2_p = 0.185
-    star3_nop = 0.023
-
-    star1_list = list(collection.find({"star": 1}, {"_id": 0}))
-    star2_list = list(collection.find({"star": 2}, {"_id": 0}))
-    star3_nop_list = list(
-        collection.find({"star": 3, "name": {"$ne": name}}, {"_id": 0})
-    )
-    pickup = collection.find_one({"name": name}, {"_id": 0})
-
-    l1 = len(star1_list)
-    l2 = len(star2_list)
-    l3 = len(star3_nop_list)
+    star_probs = [(1, 0.785), (2, 0.185), (3, 0.023), (4, 0.007)]
 
     results = []
+    cumul_probs = 0.0
 
-    for i, s1 in enumerate(star1_list):
-        result = dict(s1)
-        result["prob"] = ((i + 1) / l1) * star1_p
-        results.append(result)
+    for star, probs in star_probs:
+        if star == 3:
+            student_list = list(
+                collection.find({"star": 3, "name": {"$ne": name}}, {"_id": 0})
+            )
+        elif star == 4:
+            student_list = list(collection.find({"name": name}, {"_id": 0}))
+        else:
+            student_list = list(collection.find({"star": star}, {"_id": 0}))
+        num_student = len(student_list)
 
-    for i, s2 in enumerate(star2_list):
-        result = dict(s2)
-        result["prob"] = ((i + 1) / l2) * star2_p + star1_p
-        results.append(result)
+        print(student_list)
 
-    for i, s3 in enumerate(star3_nop_list):
-        result = dict(s3)
-        result["prob"] = ((i + 1) / l3) * star3_nop + star1_p + star2_p
+        for i, student in enumerate(student_list):
+            result = dict(student)
+            result["prob"] = ((i + 1) / num_student) * probs + cumul_probs
+            results.append(result)
 
-        results.append(result)
-
-    pickup["prob"] = 1
-    results.append(pickup)
+        cumul_probs += probs
 
     return results
