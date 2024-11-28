14bae87ee57cadbbe0e9c2cf485af003cdaf6e4c
sinclairr08 <ljhjoon00@gmail.com>
2024-11-29 00:11:07 +0900

    refactor: clarify the meaning of student recruit functions

--- backend/src/repository/recruit.py
+++ backend/src/repository/recruit.py
@@ -6,34 +6,25 @@ client = MongoClient("mongodb://localhost:27017")
 db = client["molu-q"]
 
 
-query_1 = {"isLimited": False, "name": {"$ne": "abc"}}  # name이 "abc"가 아닌 문서
-
-query = {
-    "star": 3,  # star가 3
-    "$or": [{"isLimited": False}, {"name": "abc"}],  # isLimited가 False  # name이 "abc"
-}
-
-
-def get_students(star, name=None):
+def get_star_students(star, pickup_name=None):
     collection = db["students"]
-    if star == 3 and name:
-        return list(
-            collection.find(
-                {"star": 3, "isLimited": False, "name": {"$ne": name}}, {"_id": 0}
-            )
-        )
-    elif star == 4 and name:
-        return list(collection.find({"name": name}, {"_id": 0}))
+
+    if star == 3 and pickup_name:
+        query = {"star": 3, "isLimited": False, "name": {"$ne": pickup_name}}
+    elif star == 4 and pickup_name:
+        query = {"name": pickup_name}
     else:
-        return list(collection.find({"star": star, "isLimited": False}, {"_id": 0}))
+        query = {"star": star, "isLimited": False}
+
+    return list(collection.find(query, {"_id": 0}))
 
 
-def get_probability(star_probs, name=None):
+def get_probability(star_probs, pickup_name=None):
     results = []
     cumul_probs = 0.0
 
     for star, probs in star_probs:
-        student_list = get_students(star, name)
+        student_list = get_star_students(star, pickup_name)
         num_student = len(student_list)
 
         for i, student in enumerate(student_list):
@@ -59,14 +50,14 @@ def read_recruit_probability():
     return total_probs
 
 
-def read_pickup_probability(name):
+def read_pickup_probability(pickup_name):
 
     normal_star_probs = [(1, 0.785), (2, 0.185), (3, 0.023), (4, 0.007)]
     final_star_probs = [(2, 0.97), (3, 0.023), (4, 0.007)]
 
     total_probs = {
-        "normal": get_probability(normal_star_probs, name),
-        "final": get_probability(final_star_probs, name),
+        "normal": get_probability(normal_star_probs, pickup_name),
+        "final": get_probability(final_star_probs, pickup_name),
     }
 
     return total_probs
